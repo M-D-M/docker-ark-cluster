1c4d0a75aa6fe04df781fa3818ce045004f347f6
M-D-M <m.d.mcnamara@gmail.com>
2021-09-30 21:47:28 -0400

    Removed old commented lines from control ARK.

--- controlARK.bash
+++ controlARK.bash
@@ -4,7 +4,6 @@ _ARKBINLOC="/ARK"
 _ARKBIN=${_ARKBINLOC}/ShooterGame/Binaries/Linux/ShooterGameServer
 _ARKCONFIGSTRING="${__ARKMAP}?SessionName=${__ARKINSTANCE}?Port=${__ARKPORT}?QueryPort=${__ARKQPORT}?AltSaveDirectoryName=${__ARKINSTANCE}?OverrideStructurePlatformPrevention=True -NoTransferFromFiltering -servergamelog -server -log -clusterid=${__ARKCLUSTER}"
 _ARKBACKUPDIR="/ARK_BACKUP"
-# _ARKSCREEN="${__ARKINSTANCE}-screen"
 _ARKWORLDDIR="${_ARKBINLOC}/ShooterGame/Saved"
 
 STEAM_bin=/home/steam/steamcmd/steamcmd.sh
@@ -83,7 +82,6 @@ backupARKServer() {
 	zip -qr "/tmp/${Backup_Name}_Saved.zip" ${_ARKWORLDDIR}/
 
 	echo "Copying files to remote backup..."
-	#rclone -q sync /tmp/${Backup_Name}_Saved.zip ${_ARKBACKUPDIR}
 	rsync -a "/tmp/${Backup_Name}_Saved.zip" ${_ARKBACKUPDIR}/
 
 	echo "Removing backup file..."
